fe699a6170bb86bbf2139ed10940a940efbafa3f
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2023-12-27 17:52:16 +0100

    009

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,12 +22,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: theme,
-      home: const MealsScreen(
-        title: 'Some category',
-        meals: dummyMeals,
-
-      ),
+        theme: theme,
+        home: const CategoriesScreen(),
     );
   }
 }
