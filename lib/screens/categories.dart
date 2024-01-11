dfa0fcf3b8f54b70ef64223e678bb61b3a5f8aac
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2024-01-11 20:11:06 +0100

    025c- fixed passing data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -15,7 +15,7 @@ class CategoriesScreen extends StatelessWidget {
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
